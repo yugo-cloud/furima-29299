5a277c05f26fecdcd5b36888fed70a9efb12b9cd
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-13 18:19:03 +0900

    deviseに関するビューファイルの配置

--- db/migrate/20200911092440_devise_create_users.rb
+++ db/migrate/20200911092440_devise_create_users.rb
@@ -4,7 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
+      t.string :nickname,                 null: false
+      t.string :fimily_name,              null: false
+      t.string :first_name,               null: false
+      t.string :family_name_kana,         null: false
+      t.string :first_name_kana,          null: false
+      t.string :birth_day,                null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
