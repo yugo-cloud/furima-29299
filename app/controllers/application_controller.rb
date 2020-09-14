edd77184e040722014ad14cc2ce7f9bb6001d069
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-14 12:28:05 +0900

    items_controller作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
     devise_parameter_sanitizer.permit(:sign_in)
   end
-
 end
