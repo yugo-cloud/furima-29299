a37a6036919a90bbe80a61c11fc38a2be3479dd8
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-14 14:27:12 +0900

    deviseのviewファイル作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,5 @@
 FactoryBot.define do
   factory :user do
-    nickname { "ああああ" }
-    email { Faker::Internet.free_email }
-    password { "aaaa" }
-    password_confirmation { password }
-    family_name { "山田" }
-    first_name { "太郎" }
-    family_name_kana { "ヤマダ" }
-    first_name_kana { "タロウ" }
-    birth_day { "2020-2-20" }
+    
   end
 end
