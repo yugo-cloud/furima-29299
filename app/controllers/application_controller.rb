f233e25ac05298631c6887269403d7505f91850c
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-14 12:08:13 +0900

    カラムの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+    devise_parameter_sanitizer.permit(:sign_in)
+  end
+
 end
