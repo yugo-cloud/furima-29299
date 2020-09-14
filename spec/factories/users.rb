f21d3d9b1c6069428e7d55d7cca967554be88d21
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-14 14:30:47 +0900

    factorybot記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { "Taro" }
+    email { Faker::Internet.free_email }
+    password { "1111" }
+    password_confirmation { password }
+    family_name { "山田" }
+    first_name { "太郎” }
+    family_name_kana { "ヤマダ" }
+    first_name_kana { "タロウ" }
+    birth_day { "2020-2-20" }
   end
 end
