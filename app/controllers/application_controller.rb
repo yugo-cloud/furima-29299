8bf7a3f82f4fadbef705e1fa76284bb3821821ef
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-14 17:28:09 +0900

    トップ画面に遷移するよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
     devise_parameter_sanitizer.permit(:sign_in)
+  end
+
+  private
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'yugo' && password == '29299'
+    end
   end
 end
