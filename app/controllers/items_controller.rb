98cf6424efb62b9aa6099928c654c57ecf6e156e
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-18 15:33:46 +0900

    new,createアクション、スロトングパラメーター設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,25 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if@items.save
+      redirect_to root_path
+    else
+      rernder "new"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :handling_time_id, :price, :user)
+  end
+
+
 end
