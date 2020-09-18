a180e6e974d21faf9d8b1788b2f19607127c1bc3
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-18 18:29:57 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,10 +11,10 @@ class Item < ApplicationRecord
   belongs_to :user
 
   wirh_options presence: true do
-    validates :item_name, :description, :price, length: {maximum: 40}
+    validates :item_name, :condition_id, :postage_id, :prefecture_id, :handling_time_id, length: {maximum: 40}
     validates :description, length: {maximum: 1000}
-    validates :category_id, numericality: { other_than: 1 } 
-    validates :price, numericality: {}
+    validates :category_id, numericality: { other_than: 1 message: "can't be blank"} 
+    validates :price, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
   end
 
 end
