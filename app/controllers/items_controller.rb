fd5d708bf82c52b5d4401cc6ce7baa1c4519ad6f
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-24 17:47:02 +0900

    出品者以外且つログインユーザーのみ商品購入可能に実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
   # before_action :select_item, only: [:show, :edit]
 
   def index
