d88b61590431c0bf9885ab117c79b33e8e7884de
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-25 12:45:14 +0900

    プルリクエスト改善

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,12 +25,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
-    if@item.update(item_params)
+    if @item.update(item_params)
       redirect_to root_path
     else
       render :edit
