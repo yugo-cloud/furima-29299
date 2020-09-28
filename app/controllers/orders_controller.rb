e01733ac5180c4881d3c6b252aa7f68aaa092927
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-28 18:50:21 +0900

    「出品者がURLで購入できない」実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,6 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :seller, only: [:index]
 
   def index
     @item = Item.find(params[:item_id])
@@ -18,21 +20,24 @@ class OrdersController < ApplicationController
 
 private
 
-def order_params
-  params.permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number, :item_id, :token).merge(user_id: current_user.id)
-end
+  def order_params
+    params.permit(:post_code, :prefecture, :city, :address, :building_name, :phone_number, :item_id, :token).merge(user_id: current_user.id)
+  end
 
-def pay_item
-  Payjp.api_key = "sk_test_3fd3860cd556879b1fe62d99"  # PAY.JPテスト秘密鍵
-  Payjp::Charge.create(
-    amount: @item.price,  # 商品の値段
-    card: order_params[:token],    # カードトークン
-    currency:'jpy'                 # 通貨の種類(日本円)
-  )
-end
+  def pay_item
+    Payjp.api_key = "sk_test_3fd3860cd556879b1fe62d99"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
 
+  def seller
+    @item = Item.find(params[:item_id])
+    if @item.user_id == current_user.id
+      redirect_to root_path
+    end
+  end
  
 end
-  # def order_params
-  #  params.require(:order).permit
-  # end
--- db/migrate/20200925095547_create_addresses.rb
+++ db/migrate/20200925095547_create_addresses.rb
@@ -2,12 +2,12 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
       t.string :post_code,                null: false
-      t.integer :prefecture_id,              null: false
+      t.integer :prefecture_id,           null: false
       t.string :city,                     null: false
       t.string :address,                  null: false
       t.string :building_name
       t.integer :phone_number,            null: false, uniqueness: true
-      t.references :order,            null: false, foreign_key: true
+      t.references :order,                null: false, foreign_key: true
       t.timestamps
     end
   end
