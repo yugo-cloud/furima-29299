c9f7ab917f924b6315e119d2509ba0696574a207
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-29 17:36:16 +0900

    ログアウト関連修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   # before_action :select_item, only: [:show, :edit]
   before_action :not_seller, only: [:edit, :update, :destroy]
 
