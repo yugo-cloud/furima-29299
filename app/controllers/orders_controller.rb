a797c643724cbd988db84dcbed72177b3b7def5a
yugo-cloud <yu.roadrunner@gmail.com>
2020-09-30 11:20:36 +0900

    エラー修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
   before_action :buyable, only: [:index]
   before_action :action, only: [:index, :create]
 
