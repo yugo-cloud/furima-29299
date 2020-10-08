7fad69bd93ef121c9f711d8782ea211b65f0117c
yugo-cloud <yu.roadrunner@gmail.com>
2020-10-08 10:04:07 +0900

    「次へ、前へ」ボタン実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -53,4 +53,12 @@ class Item < ApplicationRecord
     end
   end
 
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first
+  end
+
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first
+  end
+
 end
